8f431b78675f574eb956311749e6ead759dfbaba
BennyChenChuhui <benny.chen.chuhui@gmail.com>
2019-09-09 15:15:40 +0800

    Update setup_dev.sh

--- bin/setup_dev.sh
+++ bin/setup_dev.sh
@@ -22,6 +22,8 @@ oc expose svc tasks -n ${GUID}-tasks-dev
 oc create configmap tasks-config --from-literal="application-users.properties=Placeholder" --from-literal="application-roles.properties=Placeholder" -n ${GUID}-tasks-dev
 oc set volume dc/tasks --add --name=jboss-config --mount-path=/opt/eap/standalone/configuration/application-users.properties --sub-path=application-users.properties --configmap-name=tasks-config -n ${GUID}-tasks-dev
 oc set volume dc/tasks --add --name=jboss-config1 --mount-path=/opt/eap/standalone/configuration/application-roles.properties --sub-path=application-roles.properties --configmap-name=tasks-config -n ${GUID}-tasks-dev
+
+# need to add the timeout time from 1 to 15, otherwise it will fail when run it from the common grading jenkins
 oc set probe dc/tasks --readiness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=15 -n ${GUID}-tasks-dev
 oc set probe dc/tasks --liveness --get-url=http://:8080/ --initial-delay-seconds=30 --timeout-seconds=15 -n ${GUID}-tasks-dev
 
